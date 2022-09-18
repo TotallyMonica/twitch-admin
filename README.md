95a2ee479292a8753f042e74094c4fe7873b5dfd
Monica Hanson <wolfmurdurer23@gmail.com>
2022-09-17 19:30:41 -0500

    Log in to different channel

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# twitch-admin
+# Twitch Admin Panel
 An administrative control panel written in Python
+
+-----------------------------
+
+## To do - Chatbot
+ * Process who sent what
+ * Add commands
+ * Import commands from Json/CSV
+ * Join different channel
--- bot.py
+++ bot.py
@@ -10,7 +10,13 @@ with open("files/secrets.json", "r") as file:
     port = 6667
     nickname = 'majoryoshibot'
     token = secrets["oauth"]
-    channel = '#majoryoshibot'
+    # channel = '#majoryoshi'
+    print("What twich channel would you like to join?")
+    channel = input()
+
+    if channel[0] != "#"
+        channel = "#" + channel
+
     readbuffer= ""
 
     sock = socket.socket()
