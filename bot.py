9a1c1b85aee04751492e05a7d12e4fe49f71e14a
Monica Hanson <wolfmurdurer23@gmail.com>
2022-10-20 15:30:49 -0500

    Wrote most of it using TwitchIO

--- bot.py
+++ bot.py
@@ -1,186 +1,75 @@
 #!/usr/bin/env python3
-import socket
+
+from twitchio.ext import commands
 import json
-import threading
-import time
-import sys
-import os
-from datetime import datetime
-import traceback
 
-import parse.message as message
-#import parse.args as argparse
+class Bot(commands.Bot):
 
-def init(waitLength=1):
-    global PREFIX
-    global USERNAME
-    global CHANNEL
-    global twitch
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        with open('files/secrets.json', 'r') as filp:
+            secrets = json.load(filp)
+            super().__init__(token=secrets['oauth'], prefix=secrets['prefix'], initial_channels=secrets['channels'])
 
-    with open("files/secrets.json", "r") as filp:
-        secrets = json.load(filp)
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
 
-    print("Opened secrets")
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        # Send a hello back!
+        await ctx.send(f'Hello {ctx.author.name}!')
 
-    # Define login information
-    SERVER = 'irc.chat.twitch.tv'
-    PORT = 6667
-    USERNAME = secrets['username']
-    TOKEN = secrets['oauth']
-    CHANNEL = f"#{secrets['channel']}"
-    PREFIX = secrets['prefix']
-
-    try:
-    # Connect with twitch
-        twitch = socket.socket()
-        twitch.connect((SERVER, PORT))
-        print("Connected to twitch")
-
-        # Authenticate with twitch
-        twitch.send(f'PASS {TOKEN}\n'.encode('utf-8'))
-        print("Logged in with the oauth token")
-        twitch.send(f'CAP REQ :twitch.tv/tags twitch.tv/membership twitch.tv/commands\n'.encode('utf-8'))
-        print("Retrieved tags")
-        twitch.send(f'NICK {USERNAME}\n'.encode('utf-8'))
-        print(f"Username set to {USERNAME}")
-        twitch.send(f'JOIN {CHANNEL}\n'.encode('utf-8'))
-        print(f"Joined channel {CHANNEL}")
-
-    except ConnectionResetError:
-        print(f"Connection got reset, retrying in {waitLength} seconds...")
-        time.sleep(waitLength)
-        init(waitLength * 2)
-
-def sendMsg(msg, msgType="PRIVMSG"):
-    msgType = msgType.upper()
-
-    if msgType == "PONG":
-        formattedMsg = f'{msgType} :tmi.twitch.tv\n'
-    else:
-        formattedMsg = f'{msgType} {CHANNEL} :{msg}\n'
-
-    if verbose:
-        print("Sending " + formattedMsg)
-
-    twitch.send(formattedMsg.encode('utf-8'))
-
-def readCommand(chatMsg):
-    requestedCmd = ""
-    requestedArgs = []
-    # Open the commands database
-    # With its current configuration the bot can still be running and an updated database can be provided
-    with open('files/commands.json', 'r') as filp:
-        database = json.load(filp)
-
-    # Make the chat message provided friendly
-    try: 
-        requestedCmd = chatMsg['botCommand']
-    except KeyError:
-        return None
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        # Ping pong
+        await ctx.send(f'Pong')
     
-    try:
-        requestedArgs = chatMsg['botCommandParams'].split(" ")
-    except KeyError:
-        requestedArgs = []
-
-    # Check to see if an alias was used
-    for cmd in database:
-        try:
-            for alt in database[cmd]['alias']:
-                if requestedCmd == alt:
-                    if verbose: 
-                        print(f"{requestedCmd} matches alias {alt} for {cmd}")
-                    requestedCmd = cmd
-
-        except KeyError:
-            if verbose:
-                print(cmd + " has no aliases")
-
-    # Check if requested command is in the database
-    # First, remove the prefix and the newline at the end
-    if requestedCmd in database:
-        chatCmd = requestedCmd
-        output = database[chatCmd]['output']
-
-        msg = database[requestedCmd]['output']
-
-        if verbose:
-            print(f"{chatCmd} was sent")
-
-        sendMsg(msg)
-
-    elif verbose:
-        print(f"Received invalid command {chatMsg['botCommand']}")
-
-def running():
-    if logging:
-        filename = f'chat-{CHANNEL}-{datetime.now()}.log'
-
-    try:
-        while True:
-            rawResp = twitch.recv(2048)
-            resp = rawResp.decode('utf-8')
-
-            if len(resp) != 0:
-                print(f"\n{datetime.now()}: Got message")
-                chatMsg = message.parseRawMsg(resp)
-
-                if verbose:
-                    print(resp + "\n")
-                    # print(chatMsg)
-
-                # Run if a command was requested
-                if chatMsg and chatMsg['command']['command'] == "PRIVMSG":
-                    print(chatMsg)
-                    print(chatMsg['command'])
-
-                    with open(filename, 'a') as chat:
-                        chat.write(f"{datetime.now()}: {resp}\n")
-
-                print("Waiting for message...")
-                resp = None
-
-            else:
-                print("Something broke. Put your break point here!")
-                twitch.send(f'PART\n'.encode('utf-8'))
-                twitch.close()
-                time.sleep(5)
-                init()
-
-    # Catch all exceptions, if you constantly connect and disconnect Twitch thinks your DDoSing them.
-    except Exception as e:
-        print("An unexpected error occurred.")
-        print("Error:")
-        traceback.print_exc()
-        print("Would you like to keep running?")
-        userInput = input("(y/n): ")
-        if userInput.lower() == 'y':
-            print('Attempting to rerun. If this happens again you might want to check your commands json.')
-            running()
-        elif userInput.lower() == 'n':
-            print("Exiting...")
+    @commands.command()
+    async def shoutout(self, ctx: commands.Context):
+        if ' ' in ctx.message.content:
+            message = ctx.message.content.split(' ')
+            await ctx.send(f'Shoutout to {message[1]}! Check them out at https://twitch.tv/{message[1]}')
         else:
-            print("Invalid input detected. Defaulting to no.")
-            print("Exiting...")
+            await ctx.send(f'{ctx.author.name}: Provide a username')
 
-def main():
-    init()
-    running()
-    twitch.close()
+    @commands.command()
+    async def pronouns(self, ctx: commands.Context):
+        await ctx.send(f'My pronouns are she/her')
+    
+    @commands.command()
+    async def lurk(self, ctx: commands.Context):
+        await ctx.send(f'{ctx.author.name} is now in lurk mode')
 
-if __name__ == "__main__":
-    global verbose
-    global logging
+    @commands.command()
+    async def unlurk(self, ctx: commands.Context):
+        await ctx.send(f'{ctx.author.name} has left lurk mode.')
 
-    verbose = True
-    logging = True
+    @commands.command()
+    async def donate(self, ctx: commands.Context):
+        await ctx.send(f'You can donate here: https://streamelements.com/majoryoshi')
+    
+    @commands.command()
+    async def github(self, ctx: commands.Context):
+        await ctx.send(f'Check out my open source projects at https://github.com/TotallyMonica')
+    
+    @commands.command()
+    async def multitwitch(self, ctx: commands.Context):
+        streamer = "tygrbyt3"
+        await ctx.send(f'Tonight, MajorYoshi is streaming with {streamer}! Watch the both of us here at https://multitwitch.tv/majoryoshi/{streamer}')
 
-    try: 
-        main()
-    except KeyboardInterrupt:
-        print("^C received, exiting.")
-        print("Cleaning up...")
-        twitch.close()
+    @commands.command()
+    async def twitter(self, ctx: commands.Context):
+        await ctx.send(f'My twitter is https://twitter.com/yoshithemajor')
 
-        print("Thanks for using my chatbot!")
-        print("Leave any feedback on the github page, https://github.com/TotallyMonica/twitch-admin")
+    @commands.command()
+    async def youtube(self, ctx: commands.Context):
+        await ctx.send(f'My YouTube channel is MajorYoshi! Check it out at https://youtube.com/yoshithemajor')
+
+    # @commands.command()
+    # async def project(self, ctx: commands.Context):
+    #     await ctx.send(f'The project I\'m currenly working on is called Twitch Admin. You can check it out at https://github.com/TotallyMonica/twitch-admin')
+
+bot = Bot()
+bot.run()
